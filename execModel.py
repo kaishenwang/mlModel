4c8487720af72a9ea566701aa4a8c278306da5e0
kwang40 <kaishenw1995@gmail.com>
2019-03-27 14:46:01 -0500

    update

---
+++ execModel.py
@@ -0,0 +1,33 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import model_selection
+
+# load training dataset
+training_file = 'data.csv'
+dataframe = pandas.read_csv(training_file)
+# split into labels and features, assuming labels are in first column
+array = dataframe.values
+X = array[:,1:len(array[0])]
+Y = array[:,0]
+# prepare random seed for cross validation test harness
+seed = 7
+clf = RandomForestClassifier(n_estimators=500, max_depth=30, random_state=0)
+clf.fit(X, Y)
+# output feature importance
+importances = clf.feature_importances_
+indices = np.argsort(importances)[::-1]
+feat_labels = dataframe.columns[1:]
+for f in range(XT.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, feat_labels[indices[f]], importances[indices[f]]))
+
+# apply model
+dataframe = pands.read_csv('fullDataPrepared.csv')
+array = fullData.values
+predictRes = clf.predict(array[:,1:len(array[0])])
+print('Finish predicting.')
+with open('predictionResult.txt', 'w') as f:
+    for i in predictRes:
+        f.write(str(i) + '\n')
---
+++ generateFullData.py
@@ -0,0 +1,61 @@
+import csv
+
+maxATagLen = 0.0
+maxPageLen = 0.0
+for file in ['/data1/nsrg/kwang40/topDomainData/topDomainInfo.csv', '/home/kwang40/testExtractor/parkedInfo.csv']:
+    with open (file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if float(row['aTagCount']) != 0:
+                aTagLen = float(row['aTagLen']) / float(row['aTagCount'])
+                maxATagLen = max(aTagLen, maxATagLen)
+            if float(row['rawPageLen']) != 0:
+                maxPageLen = max(maxPageLen, float(row['rawPageLen']))
+
+with open ('/home/kwang40/mlModel/fullDataInfo.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    with open ('fullDataPrepared.csv', 'w') as f:
+        f.write('parked,rawPageLen,textTotalRatio,jsCodeTotalRatio,aTagLen')
+        f.write('index,follow,archive\n')
+        for row in reader:
+            f.write('1,')
+            pageLen = 0.0
+            textTotalRatio = 0.0
+            jsCodeTotalRatio = 0.0
+            if float(row['rawPageLen']) != 0:
+                pageLen = float(row['rawPageLen'])
+                textTotalRatio = (float(row['headTextLen']) + float(row['bodyTextLen'])) / pageLen
+                jsCodeTotalRatio = float(row['codeLen']) / pageLen
+            f.write(str(float(row['rawPageLen'])/maxPageLen) + ',' + str(textTotalRatio) + ',' + str(jsCodeTotalRatio) + ',')
+            aTagLen = 0
+            if float(row['aTagCount']) != 0:
+                aTagLen = float(row['aTagLen']) / float(row['aTagCount'])
+                aTagLen = aTagLen / maxATagLen
+            f.write(str(aTagLen) + ',')
+            f.write(row['index'] + ',')
+            f.write(row['follow'] + ',')
+            f.write(row['archive'] + '\n')
+
+with open ('/home/kwang40/mlModel/fullDataInfo.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    with open ('fullDataPreparedWithURL.csv', 'w') as f:
+        f.write('parked,URL,rawPageLen,textTotalRatio,jsCodeTotalRatio,aTagLen')
+        f.write('index,follow,archive\n')
+        for row in reader:
+            f.write('1,' + row['domain'] + ',')
+            pageLen = 0.0
+            textTotalRatio = 0.0
+            jsCodeTotalRatio = 0.0
+            if float(row['rawPageLen']) != 0:
+                pageLen = float(row['rawPageLen'])
+                textTotalRatio = (float(row['headTextLen']) + float(row['bodyTextLen'])) / pageLen
+                jsCodeTotalRatio = float(row['codeLen']) / pageLen
+            f.write(str(float(row['rawPageLen'])/maxPageLen) + ',' + str(textTotalRatio) + ',' + str(jsCodeTotalRatio) + ',')
+            aTagLen = 0
+            if float(row['aTagCount']) != 0:
+                aTagLen = float(row['aTagLen']) / float(row['aTagCount'])
+                aTagLen = aTagLen / maxATagLen
+            f.write(str(aTagLen) + ',')
+            f.write(row['index'] + ',')
+            f.write(row['follow'] + ',')
+            f.write(row['archive'] + '\n')

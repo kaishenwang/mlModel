303ddcb3b8a5ecb2ae1faa0f90e70bd1f4ac5acb
kwang40 <kaishenw1995@gmail.com>
2019-03-27 15:28:17 -0500

    udpate

---
+++ detectFreenom.py
@@ -0,0 +1,9 @@
+with open('/data1/nsrg/kwang40/fullData/2019-03-03/banners.json') as f:
+    with open('freenom.txt', 'w') as f2:
+        with line in f:
+            if line[:4] == 'null':
+                continue
+            try:
+                data = json.loads(line.rstrip())
+                if data['data']['http']['status_line'] == '429 Too Many Requests':
+                    f2.write(data['domain'] + '\n')

cd152a1d86441d2315eb494ae3b929fe69c10f82
kwang40 <kaishenw1995@gmail.com>
2019-03-27 15:10:17 -0500

    analysisRes.py

---
+++ analysisRes.py
@@ -0,0 +1,28 @@
+# get detected parked domains
+detected = {}
+with open('/home/kwang40/parkedDomainHeuristic/fullParkedDomains.txt') as f:
+    for line in f:
+        detected[line.rstrip()] = True
+
+# get domains
+domainList = []
+with open('fullDataPreparedWithURL.csv') as f:
+    firstLine = True
+    for line in f:
+        if firstLine:
+            firstLine = False
+            continue
+        parts = line.split(',')
+        domainList.append(parts[1])
+
+count = 0
+with open('predictionResult.txt') as f:
+    with open('newDomains.txt', 'w') as f2:
+        idx = 0
+        for line in f:
+            if line[0] == '1':
+                count += 0
+            if domainList[idx] not in detected:
+                f2.write(domainList[idx] + '\n')
+
+print ('Total Count is ' + str(count))

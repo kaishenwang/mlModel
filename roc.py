bfa5431c3dc14c34d76c5fadcf2a84a90329a8bf
Kaishen Wang <kwang40@castleblack.sprai.org>
2019-03-27 11:59:35 -0500

    update

--- roc.py
+++ roc.py
@@ -1,3 +1,9 @@
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,16 +21,27 @@ dataframe = pandas.read_csv(training_file)
 array = dataframe.values
 X = array[:,1:len(array[0])]
 Y = array[:,0]
+y = label_binarize(Y, classes=[0,1])
+n_classes = y.shape[1]
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.5,
-                                                    random_state=0)
+                                                    random_state=7)
 
 clf = RandomForestClassifier(n_estimators=500, max_depth=30, random_state=7)
 classifier = OneVsRestClassifier(clf)
-y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+y_score = classifier.fit(X_train, y_train).predict_proba(X_test)
+#print(len(X_test))
+#print(y_score)
+y_score = [x[1] for x in y_score]
+fpr = {}
+tpr = {}
+roc_auc = {}
+# Compute micro-average ROC curve and ROC area
+fpr[0], tpr[0], _ = roc_curve(y_test.ravel(), y_score)
+roc_auc[0] = auc(fpr[0], tpr[0])
 plt.figure()
 lw = 2
-plt.plot(fpr[2], tpr[2], color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot(fpr[0], tpr[0], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
 plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])

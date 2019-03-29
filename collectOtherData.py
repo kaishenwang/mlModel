a379c4483cfffb83df7718bdbdbce7f79654c4f2
kwang40 <kaishenw1995@gmail.com>
2019-03-28 20:09:54 -0500

    update

--- collectOtherData.py
+++ collectOtherData.py
@@ -13,16 +13,20 @@ with open ('otherData.csv', 'w') as f:
             continue
         with open(rdName + dNames[i] + '/log.txt') as logFile:
             if i == 0:
-                f.write('totalUrlCount,uniqueDomainCount,totalIpCount,uniqueIpCount,'
+                f.write('uniqueDomainCount,totalIpCount,uniqueIpCount,'
                         + 'uniqueOpenIpCount,domainOpenCount,urlOpenCount,success_count\n')
             lines = logFile.readlines()
             f.write(str(i) + ',')
-            for lineIdx in range(8):
+            parts = lines[0].rstrip().split(':')
+            totalCount = float(parts[1])
+            for lineIdx in range(2,8):
                 if lineIdx == 1:
                     continue
                 parts = lines[lineIdx].rstrip().split(':')
-                f.write(parts[1] + ',')
+                rate = float(parts[1]) / totalCount
+                f.write(str(rate) + ',')
         with open(rdName + dNames[i] + '/result.txt') as resultFile:
             lines = resultFile.readlines()
             data = json.loads(lines[0][1:-1])
-            f.write(str(data['success_count']) + '\n')
+            sucess_rate = float(data['success_count']) / totalCount
+            f.write(str(sucess_rate) + '\n')

352d6fbe98557470c02d80b39135d417fac6c960
Kaishen Wang <kwang40@castleblack.sprai.org>
2019-04-27 17:51:40 -0500

    update

--- collectOtherData.py
+++ collectOtherData.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-rdName = '/data1/nsrg/kwang40/fullData/'
+rdName = '/data1/nsrg/kwang40/completeData/'
 dNames = [name for name in os.listdir(rdName) if os.path.isdir(rdName + name)]
 dNames.sort()
 
@@ -13,8 +13,7 @@ with open ('otherData.csv', 'w') as f:
             continue
         with open(rdName + dNames[i] + '/log.txt') as logFile:
             if i == 0:
-                f.write('uniqueDomainCount,totalIpCount,uniqueIpCount,'
-                        + 'uniqueOpenIpCount,domainOpenCount,urlOpenCount,success_count\n')
+                f.write('time(day),Unique Domain Count,Total IP Count,Unique IP Count,Unique Open IP Count,Domain Open Count,URL Open Count,Success Grab Count\n')
             lines = logFile.readlines()
             f.write(str(i) + ',')
             parts = lines[0].rstrip().split(':')
@@ -23,10 +22,10 @@ with open ('otherData.csv', 'w') as f:
                 if lineIdx == 1:
                     continue
                 parts = lines[lineIdx].rstrip().split(':')
-                rate = float(parts[1]) / totalCount
+                rate = float(parts[1]) / totalCount * 100
                 f.write(str(rate) + ',')
         with open(rdName + dNames[i] + '/result.txt') as resultFile:
             lines = resultFile.readlines()
             data = json.loads(lines[0][1:-1])
-            sucess_rate = float(data['success_count']) / totalCount
+            sucess_rate = float(data['success_count']) / totalCount*100
             f.write(str(sucess_rate) + '\n')
